18d8a00caab3e83a36f85818e89ac8f88bdf62d8
beardofdoom <marcin.jusiak@gmail.com>
2019-01-30 17:12:02 +0100

    Write Form as a controlled component

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,38 @@ import './App.css';
 import Form from './Form/Form'
 
 class App extends Component {
+
+  state = {
+    username: '',
+    surname: '',
+    email: '',
+    password: '',
+    accept: false,
+  }
+
+  handleChange = (e) => {
+
+    const name = e.target.name;
+    const type = e.target.type;
+
+    if (type === "text" || type === "email" || type === "password") {
+      this.setState({
+        [name]: e.target.value
+      })
+    } else if (type === "checkbox") {
+      this.setState({
+        [name]: e.target.checked,
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Form />
+        <Form
+          state={this.state}
+          change={this.handleChange}
+        />
       </div>
     );
   }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,26 +1,30 @@
 import React from 'react';
 
-const Form = () => {
+const Form = (props) => {
+
+    const { username, surname, email, password, accept } = props.state;
+    console.log(username);
+
     return (
         <form noValidate>
             <label htmlFor="name">
                 Name:
-                <input type="text" id="name" />
+                <input type="text" name="username" id="name" onChange={props.change} value={username} />
             </label>
             <label htmlFor="surname">
                 Surname:
-                <input type="text" id="surname" />
+                <input type="text" name="surname" id="surname" onChange={props.change} value={surname} />
             </label>
             <label htmlFor="email">
                 E-mail:
-                <input type="email" id="email" />
+                <input type="email" name="email" id="email" onChange={props.change} value={email} />
             </label>
             <label htmlFor="password">
                 Password:
-                <input type="password" id="password" />
+                <input type="password" name="password" id="password" onChange={props.change} value={password} />
             </label>
             <label htmlFor="accept">
-                <input type="checkbox" id="accept" />
+                <input type="checkbox" name="accept" id="accept" onChange={props.change} checked={accept} />
                 I would like to receive exclusive offers that may be of interest to me.
             </label>
             <button>Register</button>
